4e048c7186be70c664d6b19d6791f5d8fe69bc42
yanggari <yanggari77@gmail.com>
2020-04-16 11:58:59 +0900

    blog app - Post

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['pk','title','created','author']
+    list_display_links = ['title']
+admin.site.register(Post,PostAdmin)
+
