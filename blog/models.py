e3af591044c65c1bc51c3912139b5880fc5d8d2c
yanggari77 <yanggari77@gmail.com>
2020-04-16 20:47:34 +0900

    show random image when head_image is none

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    head_image = models.ImageField(upload_to='blog/%y/%m/%d/',blank=True)
     created = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
