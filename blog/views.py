1cc8de5b5884933780f2337c706a366bf39ec351
yanggari <yanggari77@gmail.com>
2020-04-16 12:18:17 +0900

    blog post list(index)

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('-created')[:5]
+
+    return render(request, 'blog/index.html', {
+        'posts':posts,
+    }
+)
