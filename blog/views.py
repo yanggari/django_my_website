508b1ce03664feb219c1c2d2dc23920dbee4f520
yanggari77 <yanggari77@gmail.com>
2020-04-19 19:26:34 +0900

    categories in post_detail page

--- blog/views.py
+++ blog/views.py
@@ -19,6 +19,12 @@ class PostList(ListView):
 
 class Postdetail(DetailView):
     model=Post
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(Postdetail, self).get_context_data(**kwargs)
+        context['category_list'] = Category.objects.all()
+        context['posts_without_category'] = Post.objects.filter(category=None).count()
+
+        return context
 
 
 
