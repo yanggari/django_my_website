39a961ff5446fcdd647a6c925caf469d4d069507
yanggari77 <yanggari77@gmail.com>
2020-04-19 14:00:45 +0900

    category in post_list

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from . models import Post
+from . models import Post, Category
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['pk','title','created','author']
     list_display_links = ['title']
 admin.site.register(Post,PostAdmin)
+admin.site.register(Category)
 
--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=25,unique=True)
+    description = models.TextField(blank=True)
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -10,10 +16,14 @@ class Post(models.Model):
     head_image = models.ImageField(upload_to='blog/%y/%m/%d/',blank=True)
     created = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
     def __str__(self):
         return self.title
     def get_absolute_url(self):
         return '/blog/{}'.format(self.pk)
+
+
+
 
 
 
